ebfb0b2bdf70c216de23b43617340476d910d630
Davinaputku <davinaputri2408@gmail.com>
2024-10-20 15:10:53 +0700

    #4 controller

---
+++ app/controllers/About.php
@@ -0,0 +1,14 @@
+<?php 
+
+class About{
+    public function index($nama = 'Davina', $pekerjaan = 'Mahasiswi'){
+        echo "Halo, nama saya $nama, saya adalah $pekerjaan";
+    }
+
+    public function page(){
+        echo 'About/page';
+    }
+
+}
+
+?>
