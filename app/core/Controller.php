d25025d4cbb9f4bcb2f2e6c721d0eefe8cb09eec
Davinaputku <davinaputri2408@gmail.com>
2024-10-20 17:26:35 +0700

    #7 model

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -5,7 +5,11 @@ class Controller{
         require_once '../app/views/' . $view . '.php';
     }
 
-
+    public function model($model, $data = [])
+    {
+        require_once '../app/models/' . $model . '.php';
+        return new $model;
+    }
 }
 
 ?>
---
+++ app/models/User_model.php
@@ -0,0 +1,11 @@
+<?php 
+
+class User_model
+{
+    private $nama = 'Davina Putri Kusuma';
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
+?>
