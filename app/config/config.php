b8d09c76566d7eebeb7e6b4a1721971b40cb5a6f
Davinaputku <davinaputri2408@gmail.com>
2024-10-20 17:45:21 +0700

    Database Wrapper

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php 
+
+define('BASEURL', 'http://localhost/Prakweb2024_C_223040016_MVC/public/');
+//DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'prakweb2024_C_223040016_mvc');
+?>
--- app/core/Contants.php
+++
@@ -1,4 +0,0 @@
-<?php 
-
-define('BASEURL', 'http://localhost/Prakweb2024_C_223040016_MVC/public/');
-?>
